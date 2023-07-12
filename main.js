ee79308830a03c1ad48ec7dd5a43f5983fb16b4c
Tatiana <tati14953@gmail.com>
2023-07-11 21:39:09 -0300

    agrego menu bar mobile

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.inactive');
+const hamburguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 const toggleDesktopMenu = (event) => {
-    console.log('click');
     event.preventDefault();
-    desktopMenu.classList.toggle('inactive')
+    desktopMenu.classList.toggle('inactive');
+};
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburguerMenu.addEventListener('click', toggleMobileMenu);
