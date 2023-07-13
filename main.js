fcf0340494a69af1acbeafbf34ecddc6a3a47368
Tatiana <tati14953@gmail.com>
2023-07-13 17:45:21 -0300

    Menu desplegable mobile / desktop / menu-aside terminados con js

--- main.js
+++ main.js
@@ -1,16 +1,55 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.inactive');
 const hamburguerMenu = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu.inactive')
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.shopping-cart.inactive');
 
-const toggleDesktopMenu = (event) => {
-    event.preventDefault();
-    desktopMenu.classList.toggle('inactive');
-};
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
-}
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 hamburguerMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleCartAside);
+
+
+function toggleDesktopMenu(event) {
+    event.preventDefault();
+    
+    let asideClosed = aside.classList.contains('inactive');
+    
+    if (!asideClosed) {
+        aside.classList.add('inactive');   
+    }   
+    
+    desktopMenu.classList.toggle('inactive');
+}
+
+
+function toggleMobileMenu(){
+    let asideClosed = aside.classList.contains('inactive');
+
+    if(!asideClosed){ 
+       aside.classList.add('inactive')
+    }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside() {
+    let mobileMenuClosed = mobileMenu.classList.contains('inactive');
+    let desktopMenuClosed = desktopMenu.classList.contains('inactive')
+    
+
+    if(!mobileMenuClosed){
+        mobileMenu.classList.add('inactive')
+    }
+
+    if (!desktopMenuClosed) {
+        desktopMenu.classList.add('inactive');      
+    }
+
+    aside.classList.toggle('inactive');
+    
+ 
+    
+}
