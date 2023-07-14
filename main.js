76b7180fda03dc0e3ff77b932d6aeeb6fad636a6
Tatiana <tati14953@gmail.com>
2023-07-14 16:35:13 -0300

    fusiono productos al index. Los productos pasaron a ser objetos de javascript, los cuales mediante una fx que itera la lista de productos genera cada elemento html

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const hamburguerMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu.inactive')
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.shopping-cart.inactive');
-
+const mainContainer = document.querySelector('.cards-container')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -52,4 +52,93 @@ function toggleCartAside() {
     
  
     
+} 
+
+let productList = [];
+
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: './bike.jpeg'
+})
+productList.push({
+    name: 'Laptop',
+    price: 300,
+    image: 'https://media.gq.com.mx/photos/61e70ca25def32c5619cef06/4:3/w_712,h_534,c_limit/Lenovo%20Yoga%20Slim%207%20Pro.jpg'
+})
+productList.push({
+    name: 'Airpods',
+    price: 200,
+    image: 'https://i.blogs.es/85cc93/auriculares-airpods-1-/1366_2000.jpg'
+})
+productList.push({
+    name: 'Keyboard',
+    price: 160,
+    image: 'https://www.infinitonline.com.ar/images/000000000000011900733TGA108.jpg'
+})
+
+
+function renderProducts(array) {
+    
+for (let product of array) {
+    //creo elemento contenedor
+
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    //Creamos la etiqueta img + img del producto
+
+    const productImage = document.createElement('img');
+    productImage.classList.add('product-img')
+
+    productImage.setAttribute('src', product.image);
+
+    //Creo el contenedor de la información / con clase: product-info
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    //Div contenedor
+
+    const productInfoDiv = document.createElement('div')
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price; //texto
+
+    const productName = document.createElement('p'); //texto
+    productName.innerText = product.name;
+
+    //inserto elementos dentro dle contenedor
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    //etiqueta figure + img
+    const figureImgAddCart = document.createElement('figure');
+    const imgAddCart = document.createElement('img');
+    imgAddCart.setAttribute('src', './icons/bt_add_to_cart.svg' ); //no es una img dinámica.
+
+    figureImgAddCart.appendChild(imgAddCart); //meto la img dentro de la etiqueta figure.
+
+    
+    //insertar elementos hijos en sus contenedores para inyectarlo en el html.
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(figureImgAddCart);
+
+    productCard.appendChild(productImage);
+    productCard.appendChild(productInfo);
+
+    mainContainer.appendChild(productCard);
+
+    
+    
 }
+    
+}
+
+renderProducts(productList) //llamo la fx con el array que quiero iterar
+
+
+
+
+
